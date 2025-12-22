7aeb207cc23dccd5d51e5de26760136a8a9814fe
Konrad <git@konrad.online>
2025-12-22 14:57:45 +0100

    error on events block

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,9 @@ import pkg from "./package.json";
 
 type PkgDep = Record<string, string>;
 const { dependencies = {}, devDependencies = {} } = pkg as any as {
-	dependencies: PkgDep;
-	devDependencies: PkgDep;
-	[key: string]: unknown;
+  dependencies: PkgDep;
+  devDependencies: PkgDep;
+  [key: string]: unknown;
 };
 errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 
@@ -21,35 +21,36 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  * Note that Vite normally starts from `index.html` but the qwikCity plugin makes start at `src/entry.ssr.tsx` instead.
  */
 export default defineConfig(({ command, mode }): UserConfig => {
-	return {
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-		// This tells Vite which dependencies to pre-build in dev mode.
-		optimizeDeps: {
-			// Put problematic deps that break bundling here, mostly those with binaries.
-			// For example ['better-sqlite3'] if you use that in server functions.
-			exclude: [],
-		},
+  return {
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    // This tells Vite which dependencies to pre-build in dev mode.
+    optimizeDeps: {
+      // Put problematic deps that break bundling here, mostly those with binaries.
+      // For example ['better-sqlite3'] if you use that in server functions.
+      // Exclude native binary deps to prevent Vite from pre-bundling them in dev.
+      exclude: ["better-sqlite3"],
+    },
 
-		/**
-		 * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
-		 */
-		ssr: {
-			noExternal: ["date-fns"],
-		},
+    /**
+     * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
+     */
+    ssr: {
+      noExternal: ["date-fns"],
+    },
 
-		server: {
-			headers: {
-				// Don't cache the server response in dev mode
-				"Cache-Control": "public, max-age=0",
-			},
-		},
-		preview: {
-			headers: {
-				// Do cache the server response in preview (non-adapter production build)
-				"Cache-Control": "public, max-age=600",
-			},
-		},
-	};
+    server: {
+      headers: {
+        // Don't cache the server response in dev mode
+        "Cache-Control": "public, max-age=0",
+      },
+    },
+    preview: {
+      headers: {
+        // Do cache the server response in preview (non-adapter production build)
+        "Cache-Control": "public, max-age=600",
+      },
+    },
+  };
 });
 
 // *** utils ***
@@ -60,40 +61,40 @@ export default defineConfig(({ command, mode }): UserConfig => {
  * @param {Object} dependencies - List of production dependencies
  */
 function errorOnDuplicatesPkgDeps(
-	devDependencies: PkgDep,
-	dependencies: PkgDep,
+  devDependencies: PkgDep,
+  dependencies: PkgDep,
 ) {
-	let msg = "";
-	// Create an array 'duplicateDeps' by filtering devDependencies.
-	// If a dependency also exists in dependencies, it is considered a duplicate.
-	const duplicateDeps = Object.keys(devDependencies).filter(
-		(dep) => dependencies[dep],
-	);
+  let msg = "";
+  // Create an array 'duplicateDeps' by filtering devDependencies.
+  // If a dependency also exists in dependencies, it is considered a duplicate.
+  const duplicateDeps = Object.keys(devDependencies).filter(
+    (dep) => dependencies[dep],
+  );
 
-	// include any known qwik packages
-	const qwikPkg = Object.keys(dependencies).filter((value) =>
-		/qwik/i.test(value),
-	);
+  // include any known qwik packages
+  const qwikPkg = Object.keys(dependencies).filter((value) =>
+    /qwik/i.test(value),
+  );
 
-	// any errors for missing "qwik-city-plan"
-	// [PLUGIN_ERROR]: Invalid module "@qwik-city-plan" is not a valid package
-	msg = `Move qwik packages ${qwikPkg.join(", ")} to devDependencies`;
+  // any errors for missing "qwik-city-plan"
+  // [PLUGIN_ERROR]: Invalid module "@qwik-city-plan" is not a valid package
+  msg = `Move qwik packages ${qwikPkg.join(", ")} to devDependencies`;
 
-	if (qwikPkg.length > 0) {
-		throw new Error(msg);
-	}
+  if (qwikPkg.length > 0) {
+    throw new Error(msg);
+  }
 
-	// Format the error message with the duplicates list.
-	// The `join` function is used to represent the elements of the 'duplicateDeps' array as a comma-separated string.
-	msg = `
+  // Format the error message with the duplicates list.
+  // The `join` function is used to represent the elements of the 'duplicateDeps' array as a comma-separated string.
+  msg = `
     Warning: The dependency "${duplicateDeps.join(
-			", ",
-		)}" is listed in both "devDependencies" and "dependencies".
+      ", ",
+    )}" is listed in both "devDependencies" and "dependencies".
     Please move the duplicated dependencies to "devDependencies" only and remove it from "dependencies"
   `;
 
-	// Throw an error with the constructed message.
-	if (duplicateDeps.length > 0) {
-		throw new Error(msg);
-	}
+  // Throw an error with the constructed message.
+  if (duplicateDeps.length > 0) {
+    throw new Error(msg);
+  }
 }
